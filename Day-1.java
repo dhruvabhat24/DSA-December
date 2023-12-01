45e5a9bffc90027ecd0f85402aa5cba79d1e8dc2
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-12-01 19:57:46 +0530

    Create Day-1.java

---
+++ Day-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        int moves = 0;
+        for(char c : chars){
+            if(c == '('){
+                stack.push('(');
+            }else{
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }else{
+                    moves++;
+                }
+            }
+        }
+        return moves + stack.size();
+    }
+}
