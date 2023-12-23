9a0b99311bd80a9aed99f8ce8de62694d362abe3
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-12-23 17:59:58 +0530

    Create Day-23.java

---
+++ Day-23.java
@@ -0,0 +1,43 @@
+// time: n, space: n.
+// BFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    // Switches left and right pointers of 'root'
+    private static void invert(TreeNode root) {
+      if (root == null) return;
+      TreeNode temp = root.right;
+      root.right = root.left;
+      root.left = temp;
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+      if (root == null) return null;
+      Queue<TreeNode> q = new LinkedList<>();
+      q.add(root);
+      while (q.size() > 0) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+          TreeNode temp = q.remove();
+          invert(temp);
+          if (temp.left != null) q.add(temp.left);
+          if (temp.right != null) q.add(temp.right);
+        }
+      }
+      return root;
+    }
+}
